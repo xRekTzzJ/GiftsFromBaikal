e5d4d68da210bed1b196e9ff95366b2a96d3affa
Pavel Popov <pavel2003sbk@yandex.ru>
2025-09-01 23:16:28 +0300

    do not found page adaptive

--- frontend/src/pages/notFound/notFound.tsx
+++ frontend/src/pages/notFound/notFound.tsx
@@ -1,3 +1,4 @@
+import { useIsMobile } from "@/hooks/use-mobile";
 import { useTheme } from "@/src/store";
 const Snow = () => {
   const { theme } = useTheme();
@@ -6,7 +7,7 @@ const Snow = () => {
   return (
     <div className="pointer-events-none fixed inset-0 z-10">
       {Array.from({ length: 50 }).map((_, i) => {
-        const delay = Math.random() * 5; // случайная задержка появления
+        const delay = Math.random() * 5;
         const duration = 5 + Math.random() * 5;
 
         return (
@@ -18,7 +19,7 @@ const Snow = () => {
               backgroundColor: color,
               animationDuration: `${duration}s`,
               animationDelay: `${delay}s`,
-              opacity: 0, // начальная прозрачность
+              opacity: 0,
             }}
           />
         );
@@ -54,21 +55,74 @@ const Waves = () => (
   </div>
 );
 
-export const NotFoundPage = () => (
-  <div className="relative flex flex-col items-center justify-center h-full text-center p-4 bg-gradient-to-b from-blue-100 via-blue-50 to-white dark:from-gray-800 dark:via-gray-900 dark:to-black overflow-hidden">
-    <Snow />
-    <Waves />
-    <h1 className="text-6xl font-bold text-blue-700 dark:text-blue-300 mb-4 z-20 relative">
-      404
-    </h1>
-    <p className="text-xl text-gray-700 dark:text-gray-300 mb-6 z-20 relative">
-      Кажется, эта страница утонула в Байкале…
-    </p>
-    <a
-      href="/"
-      className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition z-20 relative"
+const Mountains = () => (
+  <div className="absolute bottom-0 w-full pointer-events-none z-0 overflow-hidden">
+    <svg
+      viewBox="0 0 1200 400"
+      className="w-full h-172 animate-parallax-mountains"
+      preserveAspectRatio="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      Вернуться на главную
-    </a>
+      <defs>
+        <linearGradient id="mountainGradientFront" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="0%" stopColor="#fff" />
+          <stop offset="20%" stopColor="#9ca3af" />
+          <stop offset="100%" stopColor="#374151" />
+        </linearGradient>
+        <linearGradient id="mountainGradientBack" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="0%" stopColor="#d1d5db" />
+          <stop offset="100%" stopColor="#1f2937" />
+        </linearGradient>
+      </defs>
+
+      {/* дальние горы */}
+      <path
+        d="M0,250 L200,150 L400,250 L600,130 L800,250 L1000,160 L1200,250 L1200,400 L0,400 Z"
+        fill="url(#mountainGradientBack)"
+      />
+    </svg>
   </div>
 );
+
+export const NotFoundPage = () => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <div className="flex flex-col mx-8 justify-center align-middle mt-auto mb-auto h-full">
+        <h1 className="text-6xl font-bold text-blue-700 dark:text-blue-300 mb-4 z-20 relative">
+          404
+        </h1>
+        <p className="text-xl text-gray-700 dark:text-gray-300 mb-6 z-20 relative">
+          Кажется, эта страница утонула в Байкале…
+        </p>
+        <a
+          href="/"
+          className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition z-20 relative"
+        >
+          Вернуться на главную
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative flex flex-col items-center justify-center h-full text-center p-4 bg-gradient-to-b from-blue-100 via-blue-50 to-white dark:from-gray-800 dark:via-gray-900 dark:to-black overflow-hidden">
+      <Snow />
+      <Mountains />
+      <Waves />
+      <h1 className="text-6xl font-bold text-blue-700 dark:text-blue-300 mb-4 z-20 relative">
+        404
+      </h1>
+      <p className="text-xl text-gray-700 dark:text-gray-300 mb-6 z-20 relative">
+        Кажется, эта страница утонула в Байкале…
+      </p>
+      <a
+        href="/"
+        className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition z-20 relative"
+      >
+        Вернуться на главную
+      </a>
+    </div>
+  );
+};
